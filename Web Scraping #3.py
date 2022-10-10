b18c010cbb0675754d8e0160767e19cc33d733fa
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-10 18:47:37 +0100

    update

--- Web Scraping #3.py
+++ Web Scraping #3.py
@@ -1,1 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('page URL').text
+soup = BeautifulSoup(page, 'html.parser')
+
+artists = soup.find_all('a')
+
+for i in artists:
+    names = artists.contents[0]
+    fullLink = artists.get('url')
+    ...
